a42a5e6a8962af9f8bba728df2060e1bf21685fa
pooriya <adibradp@gmail.com>
2024-05-03 15:21:02 +0330

    set Media on BAse url and start URL in post APP

--- djangoProject3/urls.py
+++ djangoProject3/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from .settings import MEDIA_URL,MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
