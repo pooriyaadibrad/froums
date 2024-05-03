a827d1afe77b70359cd46053d861f46a3b53cfe6
pooriya <adibradp@gmail.com>
2024-05-03 16:10:12 +0330

    the static and template tested and passed and i create some object for this test in admin panel

--- post/views.py
+++ post/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = models.post.objects.all().order_by('-id')
+
+    users = models.user.objects.all()
+    videos = models.videoclass.objects.all()
+    for user in users:
+        user.password=0
+    return render(request, 'index.html',context={'posts':posts,'users':users,'videos':videos})
+def blog(request):
+    posts = models.post.objects.all()
+    return render(request, 'blog.html',context={'posts':posts})
+def contact(request):
+    return render(request=request,template_name='Contact_us.html')
+def singlepage(request):
+    return render(request=request,template_name='single.html')
